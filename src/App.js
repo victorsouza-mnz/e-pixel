f45680486be68b74e4296fe5c982719598fc8966
victorsouza-mnz <76663041+victorsouza-mnz@users.noreply.github.com>
2021-09-08 14:45:07 -0300

    styling menu item and broke homepage into smaller components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div >
-      hello world
+      <HomePage/>
     </div>
   );
 }
